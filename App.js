d3117685e5866468ee70b9ec0eca5345fee9e585
cmoehn13 <cmoehn42@gmail.com>
2026-07-18 16:24:11 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,249 @@
+let players = [];
+
+let roster = [];
+
+
+// Load player database
+
+fetch("players.json")
+
+.then(response => response.json())
+
+.then(data => {
+
+players = data;
+
+renderRecommendations();
+
+});
+
+
+
+// Draft a player
+
+function draftPlayer(name) {
+
+
+const player =
+players.find(
+p => p.name === name
+);
+
+
+if (!player) return;
+
+
+roster.push(player);
+
+
+saveRoster();
+
+
+render();
+
+}
+
+
+
+// Save roster locally
+
+function saveRoster(){
+
+localStorage.setItem(
+"bbmRoster",
+JSON.stringify(roster)
+);
+
+}
+
+
+
+// Load previous draft
+
+function loadRoster(){
+
+const saved =
+localStorage.getItem("bbmRoster");
+
+
+if(saved){
+
+roster =
+JSON.parse(saved);
+
+}
+
+}
+
+
+
+// Display roster
+
+function renderRoster(){
+
+
+const area =
+document.getElementById("roster");
+
+
+if(roster.length === 0){
+
+area.innerHTML =
+"No players drafted yet.";
+
+return;
+
+}
+
+
+area.innerHTML =
+
+roster.map(player =>
+
+`
+
+<div class="player">
+
+<b>${player.name}</b>
+
+<br>
+
+<span class="position">
+
+${player.position}
+
+</span>
+
+-
+
+${player.team}
+
+<br>
+
+<div class="small">
+
+Stack:
+${player.stack}
+
+</div>
+
+</div>
+
+`
+
+).join("");
+
+}
+
+
+
+// Display recommendations
+
+function renderRecommendations(){
+
+
+const area =
+document.getElementById("recommendations");
+
+
+if(!area) return;
+
+
+const available =
+
+players
+
+.filter(player =>
+
+!roster.some(
+r =>
+r.name === player.name
+)
+
+)
+
+.sort(
+
+(a,b)=>
+
+b.bbmScore - a.bbmScore
+
+)
+
+.slice(0,5);
+
+
+
+area.innerHTML =
+
+
+available.map(player =>
+
+`
+
+<div class="player">
+
+<b>${player.name}</b>
+
+<br>
+
+${player.position}
+|
+${player.team}
+
+<br>
+
+<span class="score">
+
+BBM Score:
+${player.bbmScore}
+
+</span>
+
+
+<br>
+
+<div class="small">
+
+Stack:
+${player.stack}
+
+</div>
+
+<br>
+
+
+<button onclick="draftPlayer('${player.name}')">
+
+Draft Player
+
+</button>
+
+
+</div>
+
+`
+
+).join("");
+
+}
+
+
+
+// Refresh screen
+
+function render(){
+
+renderRoster();
+
+renderRecommendations();
+
+}
+
+
+
+// Start app
+
+loadRoster();
+
+render();
