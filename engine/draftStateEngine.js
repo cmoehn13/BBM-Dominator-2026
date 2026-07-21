4aa31a92bd65a1f7fab94a14bb682a63d87b067d
cmoehn13 <cmoehn42@gmail.com>
2026-07-21 18:11:52 -0500

    Update draftStateEngine.js

--- engine/draftStateEngine.js
+++ engine/draftStateEngine.js
@@ -4,47 +4,42 @@
 // =============================================
 
 let draftState = {
-
     currentRound: 1,
-
     overallPick: 1,
-
     currentTeam: 1,
-
     userDraftSlot: 1
-
 };
 
 function getDraftState() {
-
     return draftState;
-
 }
 
 function setUserDraftSlot(slot) {
-
     draftState.userDraftSlot = slot;
-
 }
 
 function getCurrentRound() {
-
     return draftState.currentRound;
-
 }
 
 function getCurrentTeam() {
-
     return draftState.currentTeam;
-
 }
 
 function getOverallPick() {
-
     return draftState.overallPick;
-
 }
 
+function calculateTeamForPick(overallPick) {
+    const round = Math.ceil(overallPick / 12);
+    const pickInRound = ((overallPick - 1) % 12) + 1;
+    if (round % 2 === 1) {
+        return pickInRound;
+    }
+    return 13 - pickInRound;
+}
+
+window.calculateTeamForPick = calculateTeamForPick;
 window.getDraftState = getDraftState;
 window.setUserDraftSlot = setUserDraftSlot;
 window.getCurrentRound = getCurrentRound;
